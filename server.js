4baa47deb37029a152195f0d452ef8f9cab10141
Felipe <felipedarosa90@gmail.com>
2026-03-27 20:42:27 -0300

    .env

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+import "dotenv/config";
 import app from './src/controllers/api.js';
 import mongoose from 'mongoose';
 
+const port = process.env.PORT || 3333;
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error("MONGO_URI nao definida no arquivo .env");
+}
+
 mongoose
-  .connect(
-    "mongodb+srv://anonymous:gVGOafHwsLdbwUGi@cluster0.nti9nti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {}
-  )
+  .connect(mongoUri, {})
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.log("Erro ao conectar", err));
 
-app.listen(3333, () => {
-  console.log('http://localhost:3333');
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 });
